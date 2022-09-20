a2a0ab69f17c105b035b2600c17213ecf535b70f
YH <spdldjqodudgp@naver.com>
2022-09-20 20:59:12 +0900

    [DOCS]HTML생성

---
+++ src/main/java/com/practice/codingStudyGroup/CodingStudyGroupApplication.java
@@ -0,0 +1,13 @@
+package com.practice.codingStudyGroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingStudyGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingStudyGroupApplication.class, args);
+	}
+
+}
